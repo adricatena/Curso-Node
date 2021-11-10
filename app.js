81ed9306af6c7bfc7a37ddf013f9d93e773f0821
Adriano <57508409+adricatena@users.noreply.github.com>
2021-11-10 12:27:26 -0300

    iniciar asyncs setup promises (2.55.0)

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home page");
+  } else if (req.url === " /about") {
+    res.end("About Page");
+  } else {
+    res.end("Error page");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
